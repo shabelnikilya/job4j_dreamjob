f415c8e71af58c4a6313df03f9ce2eade7abd8df
shabelnikilya <shabelnik_ilya@mail.ru>
2022-01-22 14:16:23 +0300

    4. candidates.jsp - список кандидатов. [#276522]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -1,9 +1,11 @@
 package ru.job4j.dream.store;
 
+import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.GregorianCalendar;
 import java.util.Map;
@@ -12,6 +14,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Store {
     private static final Store INST = new Store();
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job",
@@ -20,6 +23,9 @@ public class Store {
                 "Требуется Java разработчик с опытом от 1 года", LocalDateTime.now()));
         posts.put(3, new Post(3, "Senior Java Job",
                 "Требуется Java разработчик с опытом от 3 лет", LocalDateTime.now()));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(1, "Middle Java"));
+        candidates.put(3, new Candidate(1, "Senior Java"));
     }
 
     public static Store instOf() {
@@ -28,5 +34,9 @@ public class Store {
 
     public Collection<Post> findAll() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
